c99d9474234839376c526cfb1d6eae22fbabb211
BillTang111 <bt294@cornell.edu>
2018-07-03 13:02:40 -0400

    First commit

---
+++ src/main/java/com/BookStore/BillBookStoreApplication.java
@@ -0,0 +1,12 @@
+package com.BookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillBookStoreApplication.class, args);
+	}
+}
